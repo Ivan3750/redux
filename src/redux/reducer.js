abe09942ddbc3083fd7eef3369ab38091adcc85d
Ivan Kohan <kohan3750@gmail.com>
2025-05-04 19:29:26 +0200

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,15 +1,13 @@
 const initialState = {
-    count: 0,
-}
+  count: 0,
+};
 
 export const reducer = (state = initialState, action) => {
-    
-
-    if (action.type === "ADD"){
-        return {...state, count: state.count + action.payload}
-    }else if(action.type === "MINUS"){
-        return {...state, count: state.count - action.payload}
-    }else{
-        return state
-    }
-}
+  if (action.type === "ADD") {
+    return { ...state, count: state.count + action.payload };
+  } else if (action.type === "MINUS") {
+    return { ...state, count: state.count - action.payload };
+  } else {
+    return state;
+  }
+};
